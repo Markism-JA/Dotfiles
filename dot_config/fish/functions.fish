aef0728db44451a1b025410b06bff5589dd7da78
marky <markism1224amarille@gmail.com>
2025-09-28 03:41:27 +0800

    feat(task_view): add task cli and ui

--- dot_config/fish/functions.fish
+++ dot_config/fish/functions.fish
@@ -1,3 +1,7 @@
 function chezmtree
     tree -L 2 (chezmoi source-path)
 end
+
+function task --description "Manage and prioritize academic tasks"
+    python3 ~/Scripts/bin/check_tasks.py $argv
+end
