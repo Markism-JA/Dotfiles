a9549d51a82c7a764e45e6848a91f1bb184f8b32
marky <markism1224amarille@gmail.com>
2025-10-12 13:56:14 +0800

    update end-4 dots

--- dot_config/fish/executable_config.fish
+++ dot_config/fish/executable_config.fish
@@ -8,3 +8,10 @@ fish_vi_key_bindings
 
 #init, only works if its in config.fish
 zoxide init fish | source
+
+if test -z "$DISPLAY" -a (tty) = /dev/tty1
+    set -x XDG_SESSION_TYPE wayland
+    set -x XDG_CURRENT_DESKTOP Hyprland
+    set -x WLR_NO_HARDWARE_CURSORS 1
+    exec dbus-run-session Hyprland
+end
