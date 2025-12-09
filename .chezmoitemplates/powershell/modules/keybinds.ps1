dcec8cf63f670556ff49ac0da5d5f3238bbeff8e
marky <markism1224amarille@gmail.com>
2025-12-09 19:16:21 +0800

    fix(pwsh): vim mode changed to edit mode

--- .chezmoitemplates/powershell/modules/keybinds.ps1
+++ .chezmoitemplates/powershell/modules/keybinds.ps1
@@ -1,5 +1,6 @@
 # Function bindings
 
+Set-PSReadLineKeyHandler -Key Escape -Function ViEditVisually
 Set-PSReadLineKeyHandler -Key Ctrl+r -ScriptBlock { fzf-search-history }
 Set-PSReadLineKeyHandler -Key alt+l -ScriptBlock { fzf-search-git-log }
 Set-PSReadLineKeyHandler -Key alt+s -ScriptBlock { fzf-search-git-status }
