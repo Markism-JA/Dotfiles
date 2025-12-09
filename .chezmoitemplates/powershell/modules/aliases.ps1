ba315fb54e9b26fb91ebe40baa8c34d25613de7d
marky <markism1224amarille@gmail.com>
2025-12-09 18:43:46 +0800

    feat(pwsh): ls and l alliasing to eza (better ls)

--- .chezmoitemplates/powershell/modules/aliases.ps1
+++ .chezmoitemplates/powershell/modules/aliases.ps1
@@ -1,1 +1,11 @@
-Set-Alias l ls
+function l {
+    eza -la --icons @args
+}
+
+function ls {
+    if (Get-Command eza -ErrorAction SilentlyContinue) {
+        eza --icons @args
+    } else {
+        Get-ChildItem @args
+    }
+}
