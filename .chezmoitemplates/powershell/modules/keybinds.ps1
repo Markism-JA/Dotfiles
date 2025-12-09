37d0d9bb1db546a9142377cd0a9f4d6da95439d1
marky <markism1224amarille@gmail.com>
2025-12-09 20:09:55 +0800

    feat(pwsh): working bindings for log and status

--- .chezmoitemplates/powershell/modules/keybinds.ps1
+++ .chezmoitemplates/powershell/modules/keybinds.ps1
@@ -1,6 +1,7 @@
+Set-PSReadLineKeyHandler -Key Escape -Function ViEditVisually
+
 # Function bindings
 
-Set-PSReadLineKeyHandler -Key Escape -Function ViEditVisually
 Set-PSReadLineKeyHandler -Key Ctrl+r -ScriptBlock { fzf-search-history }
-Set-PSReadLineKeyHandler -Key alt+l -ScriptBlock { fzf-search-git-log }
-Set-PSReadLineKeyHandler -Key alt+s -ScriptBlock { fzf-search-git-status }
+Set-PSReadLineKeyHandler -Chord 'Ctrl+x,l' -ScriptBlock { fzf-search-git-log }
+Set-PSReadLineKeyHandler -Chord 'Ctrl+x,s' -ScriptBlock { fzf-search-git-status }
