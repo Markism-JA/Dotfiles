b7f1c772344f9296ac5c5dd7392b40b39045e845
marky <markism1224amarille@gmail.com>
2025-12-11 15:45:35 +0800

    fix(pwsh): sensible keys given wezterm and windows binding

--- .chezmoitemplates/powershell/modules/keybinds.ps1
+++ .chezmoitemplates/powershell/modules/keybinds.ps1
@@ -3,5 +3,5 @@ Set-PSReadLineKeyHandler -Key Escape -Function ViEditVisually
 # Function bindings
 
 Set-PSReadLineKeyHandler -Key Ctrl+r -ScriptBlock { fzf-search-history }
-Set-PSReadLineKeyHandler -Chord 'Ctrl+x,l' -ScriptBlock { fzf-search-git-log }
-Set-PSReadLineKeyHandler -Chord 'Ctrl+x,s' -ScriptBlock { fzf-search-git-status }
+Set-PSReadLineKeyHandler -Key Alt+g -ScriptBlock { fzf-search-git-log }
+Set-PSReadLineKeyHandler -Key Alt+s -ScriptBlock { fzf-search-git-status }
