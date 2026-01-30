ab937ae82d54454cbb5702e348205476d3047117
marky <markism1224amarille@gmail.com>
2026-01-30 20:04:51 +0800

    feat(fish): migrated dotnet to `~/.local/share/sdk`

--- dot_config/fish/functions/dotnet-maui.fish
+++
@@ -1,4 +0,0 @@
-function dotnet-maui --wraps=/home/marky/.dotnet-maui/dotnet --description 'alias dotnet-maui /home/marky/.dotnet-maui/dotnet'
-  /home/marky/.dotnet-maui/dotnet $argv
-        
-end
