eabd345dbacc7d258add759a571cb85acfd976cc
marky <markism1224amarille@gmail.com>
2026-05-12 10:21:41 +0800

    feat: custom startup execs

---
+++ dot_config/hypr/custom/execs.lua
@@ -0,0 +1,15 @@
+hl.on("hyprland.start", function()
+	hl.exec_cmd("albert &")
+	hl.exec_cmd("kdeconnect-indicator &")
+	hl.exec_cmd([[
+    sleep 0.7; swww img "$(cat ~/.local/state/quickshell/user/generated/wallpaper/path.txt)" \
+    --transition-type fade \
+    --transition-fps 120 \
+    --transition-duration 1 \
+    --transition-step 60
+    ]])
+	hl.exec_cmd("hyprctl setcursor Adwaita 24")
+
+	hl.exec_cmd("google-calendar-nativefier-dark")
+	hl.exec_cmd("flatpak run com.sindresorhus.Caprine")
+end)
