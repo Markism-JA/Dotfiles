06f59d758b92a7641555e0f0ee2f3289846275bd
marky <markism1224amarille@gmail.com>
2026-08-26 19:08:23 +0800

    refactor: streamlined via for loop

--- dot_config/hypr/workspaces.lua
+++ dot_config/hypr/workspaces.lua
@@ -1,59 +1,17 @@
-hl.workspace_rule({
-	workspace = "1",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
+-- Workspaces 1 to 7 on HDMI-A-1 (External Monitor)
+for ws = 1, 7 do
+	hl.workspace_rule({
+		workspace = tostring(ws),
+		monitor = "HDMI-A-1",
+		persistent = true,
+	})
+end
 
-hl.workspace_rule({
-	workspace = "2",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "3",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "4",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "5",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "6",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "7",
-	monitor = "HDMI-A-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "8",
-	monitor = "eDP-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "9",
-	monitor = "eDP-1",
-	persistent = true,
-})
-
-hl.workspace_rule({
-	workspace = "10",
-	monitor = "eDP-1",
-	persistent = true,
-})
+-- Workspaces 8 to 10 on eDP-1 (Laptop / Built-in Display)
+for ws = 8, 10 do
+	hl.workspace_rule({
+		workspace = tostring(ws),
+		monitor = "eDP-1",
+		persistent = true,
+	})
+end
