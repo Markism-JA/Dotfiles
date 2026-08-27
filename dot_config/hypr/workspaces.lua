5939e14fdb65e98d7f1df37db4269b0498d71820
marky <markism1224amarille@gmail.com>
2026-08-28 04:03:55 +0800

    feat: add default for workspace

--- dot_config/hypr/workspaces.lua
+++ dot_config/hypr/workspaces.lua
@@ -27,3 +27,53 @@ else
 		})
 	end
 end
+
+hl.workspace_rule({
+	workspace = "1",
+	layout = "master",
+})
+
+hl.workspace_rule({
+	workspace = "2",
+	layout = "monocle",
+})
+
+hl.workspace_rule({
+	workspace = "3",
+	layout = "dwindle",
+})
+
+hl.workspace_rule({
+	workspace = "4",
+	layout = "dwindle",
+})
+
+hl.workspace_rule({
+	workspace = "5",
+	layout = "dwindle",
+})
+
+hl.workspace_rule({
+	workspace = "6",
+	layout = "scrolling",
+})
+
+hl.workspace_rule({
+	workspace = "7",
+	layout = "monocle",
+})
+
+hl.workspace_rule({
+	workspace = "8",
+	layout = "scrolling",
+})
+
+hl.workspace_rule({
+	workspace = "9",
+	layout = "master",
+})
+
+hl.workspace_rule({
+	workspace = "10",
+	layout = "dwindle",
+})
