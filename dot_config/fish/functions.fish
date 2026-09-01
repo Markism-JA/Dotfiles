ec21fd2fbd866e679ad6f2cb39f267e3e621acc8
marky <markism1224amarille@gmail.com>
2026-09-01 21:21:14 +0800

    feat: login auto via function fish

--- dot_config/fish/functions.fish
+++ dot_config/fish/functions.fish
@@ -5,3 +5,14 @@ end
 function task --description "Manage and prioritize academic tasks"
     ~/Scripts/bin/task-manager/check_tasks.py $argv
 end
+
+function hl
+    if test -z "$WAYLAND_DISPLAY"
+        set -x XDG_SESSION_TYPE wayland
+        set -x XDG_CURRENT_DESKTOP Hyprland
+        set -x WLR_NO_HARDWARE_CURSORS 1
+        exec dbus-run-session Hyprland
+    else
+        echo "A Wayland session is already active."
+    end
+end
